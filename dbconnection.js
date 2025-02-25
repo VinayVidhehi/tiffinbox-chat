1eab296cae1dc0b95cd7a7e67ba51b37717f28a4
VinayVidhehi <vinayvidhehi@gmail.com>
2025-02-25 08:22:54 +0530

    connection error resolved

--- dbconnection.js
+++ dbconnection.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const SUPABASE_URL = 'https://jmggcavecybtzhvvyseb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptZ2djYXZlY3lidHpodnZ5c2ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1NDEwMjEsImV4cCI6MjA0MjExNzAyMX0.X8u43LP8NjBNC8e0mw07eOhQiGXnCI4QjbkTQ7Nyk6s'; // Paste the copied anon_key here
+const SUPABASE_URL = 'https://ymdbrndajkwatdfqmagx.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltZGJybmRhamt3YXRkZnFtYWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1NDA5ODcsImV4cCI6MjA0MjExNjk4N30.m4bmL3EzfvnPhEGyUp75nvVtc3Ptnie7OCzuhUtuwoU'; // Paste the copied anon_key here
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
